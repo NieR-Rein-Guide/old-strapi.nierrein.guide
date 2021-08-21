2354f49a24752617a312a262ea822471a6183593
Nemesis <keziah.moselle@gmail.com>
2021-08-21 21:47:57 +0200

    fix: missing comma

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       accessKeyId: env('S3_ACCESS_KEY'),
       secretAccessKey: env('S3_SECRET_KEY'),
       region: env('REGION'),
-      endpoint: `s3.${env('REGION')}.${env('S3_DOMAIN')}`
+      endpoint: `s3.${env('REGION')}.${env('S3_DOMAIN')}`,
       params: {
         Bucket: env('S3_BUCKET_NAME'),
       },
