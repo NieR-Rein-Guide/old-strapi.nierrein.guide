134d5b094ae33650d38eb1ca341b80d32e2e0dfe
Nemesis <keziah.moselle@gmail.com>
2021-08-25 01:14:09 +0200

    feat: set current authenticated user as author

--- api/guide/controllers/guide.js
+++ api/guide/controllers/guide.js
@@ -1,8 +1,29 @@
 'use strict';
 
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+   async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.article.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.article.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+};
