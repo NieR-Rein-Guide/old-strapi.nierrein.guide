f922237422761f56803859b5c09c1eee816a4bdf
Nemesis <keziah.moselle@gmail.com>
2021-08-27 02:13:16 +0200

    feat: remove special characters from slug

--- api/guide/models/guide.js
+++ api/guide/models/guide.js
@@ -6,12 +6,12 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
+        data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
+        data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
       }
     },
   },
