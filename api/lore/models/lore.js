a2324c32d3d8262a7c2c574ab818cd91abbb5c77
Nemesis <keziah.moselle@gmail.com>
2021-08-29 21:34:33 +0200

    feat: slug field stories

--- api/lore/models/lore.js
+++ api/lore/models/lore.js
@@ -5,4 +5,19 @@
  * to customize this model
  */
 
-module.exports = {};
+ const slugify = require('slugify');
+
+ module.exports = {
+   lifecycles: {
+     beforeCreate: async (data) => {
+       if (data.title) {
+         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+       }
+     },
+     beforeUpdate: async (params, data) => {
+       if (data.title) {
+         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+       }
+     },
+   },
+ };
