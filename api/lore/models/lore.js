2e11c1fd456c6f033c8cd1458267c5408b48a85b
Nemesis <keziah.moselle@gmail.com>
2021-09-04 14:46:24 +0200

    fix slug

--- api/lore/models/lore.js
+++ api/lore/models/lore.js
@@ -11,12 +11,12 @@
    lifecycles: {
      beforeCreate: async (data) => {
        if (data.title) {
-         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@?]/g })
        }
      },
      beforeUpdate: async (params, data) => {
        if (data.title) {
-         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+         data.slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@?]/g })
        }
      },
    },
